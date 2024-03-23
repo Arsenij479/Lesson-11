8d0ab7a283c8ebe14edf640c26d6caa381ed5261
Arsenij <motronyukov@bk.ru>
2024-03-23 16:32:36 +0500

    23.03.2024

---
+++ Final_Victory.py
@@ -0,0 +1,15 @@
+
+from Victory import victory_game
+print('Добро пожаловать в игру викторина')
+name = input('Как тебя зовут?')
+
+print('Отлично!', name)
+answer = input('И так ты готов?:')
+
+if answer == 'Да':
+    victory_game()
+elif answer == 'Нет':
+    print(('Ну давай готовься тогда)'))
+else:
+    print('Не могу понять, что ты хочешь от меня)')
+
